0c10dbe9a3cebc3ca148bdc469ceff74a109a26e
ili444 <39112695+ili444@users.noreply.github.com>
2019-09-18 22:38:32 +0700

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 from .models import Post
 from .parser import *
 
 
 def posts_list(request):
-    start(request)
     urls = Post.objects.all().order_by('timeshift').values()
     return render(request, 'blog/index.html', context={'urls': urls})
 
+
+def parser(request):
+    start(request)
+    return redirect('http://localhost:8000/')
+
+
 def view_post(request):
-    path = request.path[:-1] if request.path[-1:] == '/' else request.path
-    url = 'http://localhost:8000' + path
+    url = request.build_absolute_uri()[:-1] if request.build_absolute_uri()[-1:] == '/' else request.build_absolute_uri()
     date = Post.objects.filter(url=url).values()
     if date:
         return render(request, 'blog/page_post.html', context={'date': date})
